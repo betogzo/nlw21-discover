ce703906e65bf16fa27aa384802a2278443a8d6c
user <user@users-iMac.local>
2021-06-24 15:01:31 -0300

    fim quarto dia. implementação db

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -6,13 +6,34 @@ const checkButtons = document.querySelectorAll('.check');
 const deleteButtons = document.querySelectorAll('.delete');
 const cancelButton = document.querySelector('.cancel');
 
+const modalTitle = document.querySelector('.modal h2');
+const modalDescription = document.querySelector('.modal p');
+const modalButton = document.querySelector('.modal button');
+
+cancelButton.addEventListener('click', modal.close);
 
 checkButtons.forEach(button => {
-    button.addEventListener('click', e => modal.open());
+    button.addEventListener('click', event => handleClick(event));
 });
 
 deleteButtons.forEach(button => {
-    button.addEventListener('click', e => modal.open());
+    button.addEventListener('click', event => handleClick(event, false));
 });
 
-cancelButton.addEventListener('click', modal.close);
+function handleClick(event, check = true) {
+    event.preventDefault();
+
+    const roomId = document.querySelector('#room-id').dataset.id;
+    const form = document.querySelector('.modal form');
+    const slug = check ? 'check' : 'delete';
+    const questionId = event.target.dataset.id;
+
+    form.setAttribute('action', `/question/${roomId}/${questionId}/${slug}`);
+
+    modalTitle.innerHTML = check ? 'Marcar como lida' : 'Excluir pergunta';
+    modalDescription.innerHTML = check ? 'Deseja marcar esta pergunta como lida?' : 'Tem certeza que deseja excluir esta pergunta?';
+    modalButton.innerHTML = check ? 'Marcar como lida' : 'Sim, excluir';
+    check ? modalButton.classList.remove('red') : modalButton.classList.add('red');
+    modal.open();
+}
+
